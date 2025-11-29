ef49426488a121f460dd2efe9920558f6a70677e
Leg Meat <198740067+Leg-Meat@users.noreply.github.com>
2025-11-29 12:51:22 +0000

    added to play domain

--- hack-sheffield-backend/src/main/java/com/example/hack_sheffield_backend/domain/Player.java
+++ hack-sheffield-backend/src/main/java/com/example/hack_sheffield_backend/domain/Player.java
@@ -6,6 +6,16 @@ import jakarta.persistence.*;
 public class Player {
     @Id
     private int id;
+    private int hitPoints;
+    private int pooCounter;
+
+    private void setPooCounter (int pooCounter) { this.pooCounter = pooCounter; }
+
+    private int getPooCounter() { return pooCounter; }
+
+    private void setHitPoints(int hitPoints) { this.hitPoints = hitPoints; }
+
+    private int getHitPoints() { return hitPoints; }
 
     public void setId(int id) {
         this.id = id;
