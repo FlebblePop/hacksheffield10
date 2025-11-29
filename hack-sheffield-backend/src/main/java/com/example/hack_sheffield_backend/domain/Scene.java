61aea01cac899d08be67ad41132436268673156e
tomspk <tompsk@outlook.com>
2025-11-29 16:23:43 +0000

    Added Scene class and corresponding repo, service etc

---
+++ hack-sheffield-backend/src/main/java/com/example/hack_sheffield_backend/domain/Scene.java
@@ -0,0 +1,59 @@
+package com.example.hack_sheffield_backend.domain;
+
+import com.example.hack_sheffield_backend.dto.SceneDTO;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.List;
+
+@Entity
+public class Scene {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String imagePath;
+    private List<Character> characters;
+
+    public Scene() {}
+
+    public Scene(String imagePath, List<Character> characters) {
+        this.imagePath = imagePath;
+        this.characters = characters;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public List<Character> getCharacters() {
+        return characters;
+    }
+
+    public void setCharacters(List<Character> characters) {
+        this.characters = characters;
+    }
+
+    public SceneDTO toDto() {
+        SceneDTO sceneDto = new SceneDTO();
+
+        sceneDto.setId(this.id);
+        sceneDto.setImagePath(this.imagePath);
+
+        return sceneDto;
+    }
+}
---
+++ hack-sheffield-backend/src/main/java/com/example/hack_sheffield_backend/repository/SceneRepository.java
@@ -0,0 +1,7 @@
+package com.example.hack_sheffield_backend.repository;
+
+import com.example.hack_sheffield_backend.domain.Scene;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SceneRepository extends JpaRepository<Scene, Integer> {
+}
