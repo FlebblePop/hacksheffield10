3e88436ede2d2c372c3c044cd88ca9228e1e19f5
Leg Meat <198740067+Leg-Meat@users.noreply.github.com>
2025-11-29 12:17:54 +0000

    added springboot project

---
+++ hack-sheffield-backend/src/test/java/com/example/hack_sheffield_backend/HackSheffieldBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hack_sheffield_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackSheffieldBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
