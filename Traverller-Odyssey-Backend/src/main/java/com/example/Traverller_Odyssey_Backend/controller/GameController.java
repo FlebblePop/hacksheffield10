3276426828ad17ec13aff3e411b789b2c9a004d4
Owen Potter <OMPotter1@sheffield.ac.uk>
2025-11-29 23:41:36 +0000

    added processInput in GameService

--- Traverller-Odyssey-Backend/src/main/java/com/example/Traverller_Odyssey_Backend/controller/GameController.java
+++ Traverller-Odyssey-Backend/src/main/java/com/example/Traverller_Odyssey_Backend/controller/GameController.java
@@ -1,6 +1,10 @@
 package com.example.Traverller_Odyssey_Backend.controller;
 
+import com.example.Traverller_Odyssey_Backend.domain.Player;
+import com.example.Traverller_Odyssey_Backend.dto.PlayerDTO;
+import com.example.Traverller_Odyssey_Backend.services.GameService;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +14,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping({"", "/"})
 public class GameController {
 
-    public GameController() {}
+    private final GameService gameService;
+
+    @Autowired
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
 
     @GetMapping({"", "/"})
     public @ResponseBody String getScene() {
@@ -19,6 +28,8 @@ public class GameController {
 
     @GetMapping("/{input}")
     public @ResponseBody String processInput(@PathVariable String input) {
+        gameService.processInput(input);
+
         return input;
     }
 
--- Traverller-Odyssey-Backend/src/main/java/com/example/Traverller_Odyssey_Backend/services/GameService.java
+++ Traverller-Odyssey-Backend/src/main/java/com/example/Traverller_Odyssey_Backend/services/GameService.java
@@ -1,5 +1,16 @@
 package com.example.Traverller_Odyssey_Backend.services;
 
+import com.example.Traverller_Odyssey_Backend.domain.Player;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GameService {
-    public
+    public String processInput(String input) {
+
+        if (input.contains("poo")) {
+            player.setPooCounter(player.getPooCounter() + 1);
+            return "You have pood. Your Poo Counter has been incremented. It is now: " + player.getPooCounter();
+        }
+        return input;
+    }
 }
