a0bcc7c8e5ebb64a57ee3e1639209ea4986f7c58
RubberDuckBug <110607574+errorfourzero4@users.noreply.github.com>
2025-11-29 22:59:20 +0000

    Works?

--- Traverller-Odyssey-Backend/src/main/java/com/example/Traverller_Odyssey_Backend/controller/GameController.java
+++ Traverller-Odyssey-Backend/src/main/java/com/example/Traverller_Odyssey_Backend/controller/GameController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping({"", "/"})
 public class GameController {
-
-    public GameController() {}
 
     @GetMapping("/{input}")
     public ResponseEntity<> processInput(@PathVariable String input) {}
