19e90ce2e469b008a9a2d65d535940c3ac771bcb
tomspk <tompsk@outlook.com>
2025-11-30 01:40:15 +0000

    Fixed scene not changing error and removed unnecessary static

--- Traverller-Odyssey-Backend/src/main/java/com/example/Traverller_Odyssey_Backend/services/GameService.java
+++ Traverller-Odyssey-Backend/src/main/java/com/example/Traverller_Odyssey_Backend/services/GameService.java
@@ -6,61 +6,63 @@ import org.springframework.stereotype.Service;
 @Service
 public class GameService {
 
+    private final GameState gameState = new GameState();
+
     public String getIntroText() {
-        return GameState.getCurrentScene().getIntroText();
+        return gameState.getCurrentScene().getIntroText();
     }
 
     public String processInput(String input) {
 
-        if (GameState.getCurrentScene().getId() == 0) {
+        if (gameState.getCurrentScene().getId() == 0) {
             if (!input.isEmpty()) {
-                GameState.getPlayer().setName(input);
-                GameState.goToNextScene();
-                System.out.println(GameState.getCurrentScene().getId());
+                gameState.getPlayer().setName(input);
+                gameState.goToNextScene();
+                System.out.println(gameState.getCurrentScene().getId());
 
                 return "Hello " + input;
             } else {
                 return "Please enter your name";
             }
         } else if (input.contains("poo")) {
-            GameState.getPlayer().setPooCounter(GameState.getPlayer().getPooCounter() + 1);
+            gameState.getPlayer().setPooCounter(gameState.getPlayer().getPooCounter() + 1);
             return "You have pood. Your Poo Counter has been incremented. It is now: "
-                    + GameState.getPlayer().getPooCounter();
-        } else if (GameState.getCurrentScene().getId() == 1) {
+                    + gameState.getPlayer().getPooCounter();
+        } else if (gameState.getCurrentScene().getId() == 1) {
             if (input.isEmpty()) {
                 return "Please enter an action";
             } else if (input.equals("next scene")) {
-                GameState.goToNextScene();
+                gameState.goToNextScene();
 
                 return "changing to scene 2";
             } else {
                 return "Unrecognised action";
             }
-        } else if (GameState.getCurrentScene().getId() == 2) {
+        } else if (gameState.getCurrentScene().getId() == 2) {
             if (input.isEmpty()) {
                 return "Please enter an action";
             } else if (input.equals("next scene")) {
-                GameState.goToNextScene();
+                gameState.goToNextScene();
 
                 return "changing to scene 3";
             } else {
                 return "Unrecognised action";
             }
-        } else if (GameState.getCurrentScene().getId() == 3) {
+        } else if (gameState.getCurrentScene().getId() == 3) {
             if (input.isEmpty()) {
                 return "Please enter an action";
             } else if (input.equals("next scene")) {
-                GameState.goToNextScene();
+                gameState.goToNextScene();
 
                 return "changing to scene 4";
             } else {
                 return "Unrecognised action";
             }
-        } else if (GameState.getCurrentScene().getId() == 4) {
+        } else if (gameState.getCurrentScene().getId() == 4) {
             if (input.isEmpty()) {
                 return "Please enter an action";
             } else if (input.equals("next scene")) {
-                GameState.goToNextScene();
+                gameState.goToNextScene();
 
                 return "changing to scene 5";
             } else {
@@ -72,6 +74,6 @@ public class GameService {
     }
 
     public String getImagePath() {
-        return GameState.getCurrentScene().getImagePath();
+        return gameState.getCurrentScene().getImagePath();
     }
 }
