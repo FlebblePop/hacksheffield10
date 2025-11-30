8a7a03c44b169043e71c8ccba646a83a275e765d
tomspk <tompsk@outlook.com>
2025-11-30 00:16:13 +0000

    Implemented dummy logic for scenes after 0, to move from one scene to the next

--- Traverller-Odyssey-Backend/src/main/java/com/example/Traverller_Odyssey_Backend/services/GameService.java
+++ Traverller-Odyssey-Backend/src/main/java/com/example/Traverller_Odyssey_Backend/services/GameService.java
@@ -16,15 +16,55 @@ public class GameService {
             if (!input.isEmpty()) {
                 GameState.getPlayer().setName(input);
                 GameState.goToNextScene();
+
+                return "Hello " + input;
             } else {
                 return "Please enter your name";
             }
-        }
-
-        if (input.contains("poo")) {
+        } else if (input.contains("poo")) {
             GameState.getPlayer().setPooCounter(GameState.getPlayer().getPooCounter() + 1);
             return "You have pood. Your Poo Counter has been incremented. It is now: "
                     + GameState.getPlayer().getPooCounter();
+        } else if (GameState.getCurrentScene().getId() == 1) {
+            if (input.isEmpty()) {
+                return "Please enter an action";
+            } else if (input.equals("next scene")) {
+                GameState.goToNextScene();
+
+                return "changing to scene 2";
+            } else {
+                return "Unrecognised action";
+            }
+        } else if (GameState.getCurrentScene().getId() == 2) {
+            if (input.isEmpty()) {
+                return "Please enter an action";
+            } else if (input.equals("next scene")) {
+                GameState.goToNextScene();
+
+                return "changing to scene 3";
+            } else {
+                return "Unrecognised action";
+            }
+        } else if (GameState.getCurrentScene().getId() == 3) {
+            if (input.isEmpty()) {
+                return "Please enter an action";
+            } else if (input.equals("next scene")) {
+                GameState.goToNextScene();
+
+                return "changing to scene 4";
+            } else {
+                return "Unrecognised action";
+            }
+        } else if (GameState.getCurrentScene().getId() == 4) {
+            if (input.isEmpty()) {
+                return "Please enter an action";
+            } else if (input.equals("next scene")) {
+                GameState.goToNextScene();
+
+                return "changing to scene 5";
+            } else {
+                return "Unrecognised action";
+            }
         }
 
         return input;
